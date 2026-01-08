72f7cdb7681cb86fa4c8282fcbafa420b679c3da
pravin080317 <35328578+pravin080317@users.noreply.github.com>
2026-01-08 18:24:46 +0000

    c

---
+++ temp/DetailsListVOA/generated/ManifestTypes.d.ts
@@ -0,0 +1,27 @@
+/*
+*This is auto generated from the ControlManifest.Input.xml file
+*/
+
+// Define IInputs and IOutputs Type. They should match with ControlManifest.
+export interface IInputs {
+    canvasScreenName: ComponentFramework.PropertyTypes.StringProperty;
+    pageSize: ComponentFramework.PropertyTypes.WholeNumberProperty;
+    columnDisplayNames: ComponentFramework.PropertyTypes.StringProperty;
+    columnConfig: ComponentFramework.PropertyTypes.StringProperty;
+    searchBy: ComponentFramework.PropertyTypes.StringProperty;
+    billingAuthorities: ComponentFramework.PropertyTypes.StringProperty;
+    caseworkers: ComponentFramework.PropertyTypes.StringProperty;
+    workThat: ComponentFramework.PropertyTypes.StringProperty;
+    fromDate: ComponentFramework.PropertyTypes.StringProperty;
+    toDate: ComponentFramework.PropertyTypes.StringProperty;
+    searchTrigger: ComponentFramework.PropertyTypes.StringProperty;
+    allowColumnReorder: ComponentFramework.PropertyTypes.TwoOptionsProperty;
+    perfLogsEnabled: ComponentFramework.PropertyTypes.TwoOptionsProperty;
+}
+export interface IOutputs {
+    selectedTaskId?: string;
+    selectedSaleId?: string;
+    saleDetails?: string;
+    selectedTaskIdsJson?: string;
+    selectedSaleIdsJson?: string;
+}
---
+++ temp/DetailsListVOA/generated/ManifestTypes.ts
@@ -0,0 +1,19 @@
+export interface IInputs {
+    canvasScreenName: ComponentFramework.PropertyTypes.StringProperty;
+    pageSize: ComponentFramework.PropertyTypes.WholeNumberProperty;
+    columnDisplayNames: ComponentFramework.PropertyTypes.StringProperty;
+    columnConfig: ComponentFramework.PropertyTypes.StringProperty;
+    searchBy: ComponentFramework.PropertyTypes.StringProperty;
+    billingAuthorities: ComponentFramework.PropertyTypes.StringProperty;
+    caseworkers: ComponentFramework.PropertyTypes.StringProperty;
+    workThat: ComponentFramework.PropertyTypes.StringProperty;
+    fromDate: ComponentFramework.PropertyTypes.StringProperty;
+    toDate: ComponentFramework.PropertyTypes.StringProperty;
+    searchTrigger: ComponentFramework.PropertyTypes.StringProperty;
+}
+
+export interface IOutputs {
+    selectedTaskId?: string;
+    selectedSaleId?: string;
+    saleDetails?: string;
+}
